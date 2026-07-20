c57869cdb21ca6797e6630c441149382146e6d1a
SlayerArman <cartoontv123360@gmail.com>
2026-07-20 10:02:45 +0500

    ReStructured

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+console.log("FocusFlow Started");
+const view = document.getElementById("view");
+view.innerHTML = `
+<h2>Welcome 👋</h2>
+<p>
+This productivity dashboard is under construction.
+</p>
+`;
